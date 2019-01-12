5a419a647ccc738a707403acc8fec21ab75c45ca
stihova <39700980+stihova@users.noreply.github.com>
2019-01-12 23:35:39 +0200

    Location Views

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -1,7 +1,82 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User6ycee
- * Date: 9.1.2019 г.
- * Time: 22:14
- */
+@extends('layouts.app')
+        <!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Guest Houses</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .links > a {
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+    </style>
+</head>
+<body>
+<h1>Creation form</h1>
+<div class="panel-body">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form method="post" action="{{url('/locations')}}">
+        <div class="form-group row">
+            {{csrf_field()}}
+            <label for="locationName" class="col-sm-2 col-form-label col-form-label-lg">Location Name</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control form-control-lg" id="lgFormGroupInput1" placeholder="Enter Location:" name="locationName">
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-2"></div>
+            <input type="submit" class="btn btn-primary">
+            <div class="col-xl-1"></div>
+            <a class="btn btn-primary" href="{{ route('locations.index') }}"> Back</a>
+        </div>
+    </form>
+</div>
+</body>
+</html>
--- resources/views/locations/edit.blade.php
+++ resources/views/locations/edit.blade.php
@@ -4,4 +4,84 @@
  * User: User6ycee
  * Date: 9.1.2019 г.
  * Time: 22:15
- */
+ */@extends('layouts.app')
+        <!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Guest Houses</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .links > a {
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+    </style>
+</head>
+<body>
+<h1>Edit</h1>
+<div class="panel-body">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form method="post" action="{{url('/locations')}}">
+        <div class="form-group row">
+            {{csrf_field()}}
+            <label for="locationName" class="col-sm-2 col-form-label col-form-label-lg">Location Name</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control form-control-lg" id="lgFormGroupInput1" placeholder="Enter location:" name="locationName">
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-2"></div>
+            <button type="submit" class="btn-outline-dark">Update</button>
+        </div>
+    </form>
+</div>
+
+
+</body>
+</html>
